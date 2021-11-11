1bd34e87b51557c3a427c190bb546fec77930f59
doomkinn-geek <doomkinn@yandex.ru>
2021-11-11 14:33:05 +0300

    init

---
+++ Digits/Digit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digits
+{
+    public class Digit
+    {
+        private int _numerator;
+        private int _denominator;
+        public int Numerator
+        {
+            get => _numerator;
+        }
+        public int Denominator
+        {
+            get => _denominator;
+        }
+            
+    }
+}
